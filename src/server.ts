46859b85f080c9391ca9b6e6bbae5568c7624eb1
Ilya Kozlov <ilya@kozlov.dev>
2020-11-25 19:59:06 +0600

    refactor: add controllers and decorators

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import {AppRouter} from './controllers/app-router';
+import './controllers/auth.controller';
+import './controllers/root.controller';
 
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.get('/cats', (req, res) => {
-  res.send('This is cats');
-});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({keys: ['key']}));
+app.use(AppRouter.instance);
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
