3d859fca3256dd44cd116e46e960a0d042df079a
Ilya Kozlov <ilya@kozlov.dev>
2020-11-25 20:06:41 +0600

    feat(app router): add AppRouter for singleton realisation

---
+++ src/controllers/app-router.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+export class AppRouter {
+  private static _instance: express.Router;
+
+  static get instance(): express.Router {
+    if (!AppRouter._instance) {
+      AppRouter._instance = express.Router();
+    }
+    return AppRouter._instance;
+  }
+}
