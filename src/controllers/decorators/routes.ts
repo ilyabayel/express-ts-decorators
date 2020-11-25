d4138ce5a87667e1b080da281618b7c2a17e97db
Ilya Kozlov <ilya@kozlov.dev>
2020-11-25 23:14:08 +0600

    feat(decorators): add all Http Request Types Decorators

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -8,12 +8,12 @@ function routeBinder(method: string) {
   };
 }
 
-export const Get = routeBinder(HttpMethods.get);
-export const Head = routeBinder(HttpMethods.head);
-export const Post = routeBinder(HttpMethods.post);
-export const Put = routeBinder(HttpMethods.put);
-export const Patch = routeBinder(HttpMethods.patch);
-export const Delete = routeBinder(HttpMethods.delete);
-export const Connect = routeBinder(HttpMethods.connect);
-export const Options = routeBinder(HttpMethods.options);
-export const Trace = routeBinder(HttpMethods.trace);
+export const Get = routeBinder(HttpMethods.Get);
+export const Head = routeBinder(HttpMethods.Head);
+export const Post = routeBinder(HttpMethods.Post);
+export const Put = routeBinder(HttpMethods.Put);
+export const Patch = routeBinder(HttpMethods.Patch);
+export const Delete = routeBinder(HttpMethods.Delete);
+export const Connect = routeBinder(HttpMethods.Connect);
+export const Options = routeBinder(HttpMethods.Options);
+export const Trace = routeBinder(HttpMethods.Trace);
