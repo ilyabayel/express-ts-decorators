5883bcbd2aab6d62c18cd1cef2eeed9bc2e8a9a6
Ilya Kozlov <ilya@kozlov.dev>
2020-11-25 23:18:29 +0600

    feat(controllers): add login failed message

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from 'express';
-import {Get, Controller, Post} from './decorators';
+import {Get, Controller, Post, BodyValidator} from './decorators';
 import {StatusCodes} from 'http-status-codes';
 
 @Controller('/auth')
@@ -22,13 +22,29 @@ class AuthController {
   }
 
   @Post('/login')
+  @BodyValidator('email', 'password')
   postLogin(req: Request, res: Response) {
     const {email, password} = req.body;
     if (email && password && email === 'ilya' && password === 'ilya') {
       req.session = {loggedIn: true};
       res.redirect('/');
     } else {
-      res.status(StatusCodes.BAD_REQUEST).send('Email and password does not match!');
+      res.status(StatusCodes.UNAUTHORIZED).send(`
+        Email and password does not match!
+        You will be redirected after <span id="counter">3</span> secs.
+        <script>
+          let count = 3;
+          const counter = document.querySelector("#counter")
+          setInterval(() => {
+            if (count > 1) {
+              count--;
+              counter.innerHTML = count.toString();
+              return;
+            }
+            window.location.href = "http://localhost:8000/"
+          }, 1000)
+        </script>
+      `);
     }
   }
 
