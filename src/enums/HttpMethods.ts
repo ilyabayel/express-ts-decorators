1aea71afa34459d6aa061c8f14df091399695bc6
Ilya Kozlov <ilya@kozlov.dev>
2020-11-25 19:59:51 +0600

    feat(enums): add HttpMethods

---
+++ src/enums/HttpMethods.ts
@@ -0,0 +1,11 @@
+export enum HttpMethods {
+  get = 'get',
+  head = 'head',
+  post = 'post',
+  put = 'put',
+  patch = 'patch',
+  delete = 'delete',
+  connect = 'connect',
+  options = 'options',
+  trace = 'trace'
+}
